4d2a9988aa660678babbaf1b7256a14d0abdbd33
kingebrahimopq-create <kingebrahimopq@example.com>
2026-06-12 23:04:22 +0000

    feat: إضافة أدوات المساعدة (utils)، مكونات UI المتبقية، الأصول (assets)، وميثاق السلوك

---
+++ assets/index.ts
@@ -0,0 +1,5 @@
+export { default as Logo } from './logo.svg';
+export { default as LogoIcon } from './logo-icon.svg';
+export { default as TestTubeIcon } from './icon-test-tube.svg';
+export { default as MicroscopeIcon } from './icon-microscope.svg';
+export { default as ReportIcon } from './icon-report.svg';
